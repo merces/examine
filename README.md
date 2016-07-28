73c9d010205f185bddf6169c7c2217a794902c05
Fernando Mercês <nandu88@gmail.com>
2016-07-27 23:15:14 -0300

    Float calculation fix

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-examine 1.0
-===========
+eXamine 1.0 beta
+================
 
-Data conversion/examination command line tool
+Data conversion/examination command line tool.
 
 ###1. Build
     $ make
@@ -21,33 +21,29 @@ A symbolic link called "x" is also created for convenience.
 
 Analyse the hexadecimal number 0xB0B0CA:
 
-    $ $ echo 0xb0b0ca | x
-	[*] Handling as string:
+    $ echo 0xb0b0ca | x
 	Hex string: 3078623062306361
 	Byte array: \x30\x78\x62\x30\x62\x30\x63\x61
 	URL encoding: %30%78%62%30%62%30%63%61
 	ROT13: 0ko0o0pn
-	[*] Handling as number:
 	Signed int: 11579594
 	Unsigned int: 11579594
-	Float: 1.15796e+07
+	Float: 1.62265e-38
 	Hexadecimal: 0xb0b0ca
 	Octal: 054130312
 	Binary: 0b101100001011000011001010
 	RGB: (176, 176, 202)
 	IPv4: 0.176.176.202
-	Date/time stamp: Fri, 15 May 1970 00:33:14 UTC/GMT-0
+	Timestamp: Fri, 15 May 1970 00:33:14 UTC/GMT-0
 
-examine does not really know if input is intended to be string or number. It just process it as it was both and output some handy conversions. The next example shows how to use examine on interactive mode:
+eXamine does not really know whether input is intended to be string or number. It just processes it as it was both and output some handy conversions. The next example shows eXamine in interactive mode:
 
     $ x
-    examine rocks!
-	[*] Handling as string:
-	Hex string: 6578616d696e6520726f636b7321
-	Byte array: \x65\x78\x61\x6d\x69\x6e\x65\x20\x72\x6f\x63\x6b\x73\x21
-	URL encoding: %65%78%61%6D%69%6E%65%20%72%6F%63%6B%73%21
-	ROT13: rknzvar ebpxf!
-	[*] Handling as number:
+	examine rocks \m/
+	Hex string: 6578616d696e6520726f636b73205c6d2f
+	Byte array: \x65\x78\x61\x6d\x69\x6e\x65\x20\x72\x6f\x63\x6b\x73\x20\x5c\x6d\x2f
+	URL encoding: %65%78%61%6D%69%6E%65%20%72%6F%63%6B%73%20%5C%6D%2F
+	ROT13: rknzvar ebpxf \z/
 	Signed int: 0
 	Unsigned int: 0
 	Float: 0
@@ -57,25 +53,21 @@ examine does not really know if input is intended to be string or number. It jus
 	ASCII char: NUL
 	RGB: (0, 0, 0)
 	IPv4: 0.0.0.0
-	Date/time stamp: Thu, 01 Jan 1970 00:00:00 UTC/GMT-0
-
-	as long as you know what you are doing!
-	[*] Handling as string:
-	Hex string: 6173206c6f6e6720617320796f75206b6e6f77207768617420796f752061726520646f696e6721
-	Byte array: \x61\x73\x20\x6c\x6f\x6e\x67\x20\x61\x73\x20\x79\x6f\x75\x20\x6b\x6e\x6f\x77\x20\x77\x68\x61\x74\x20\x79\x6f\x75\x20\x61\x72\x65\x20\x64\x6f\x69\x6e\x67\x21
-	URL encoding: %61%73%20%6C%6F%6E%67%20%61%73%20%79%6F%75%20%6B%6E%6F%77%20%77%68%61%74%20%79%6F%75%20%61%72%65%20%64%6F%69%6E%67%21
-	ROT13: nf ybat nf lbh xabj jung lbh ner qbvat!
-	[*] Handling as number:
-	Signed int: 0
-	Unsigned int: 0
-	Float: 0
-	Hexadecimal: 00
-	Octal: 0
-	Binary: 0b
-	ASCII char: NUL
-	RGB: (0, 0, 0)
-	IPv4: 0.0.0.0
-	Date/time stamp: Thu, 01 Jan 1970 00:00:00 UTC/GMT-0
+	Timestamp: Thu, 01 Jan 1970 00:00:00 UTC/GMT-0
+	
+	0x3fc00000
+	Hex string: 30783366633030303030
+	Byte array: \x30\x78\x33\x66\x63\x30\x30\x30\x30\x30
+	URL encoding: %30%78%33%66%63%30%30%30%30%30
+	ROT13: 0k3sp00000
+	Signed int: 1069547520
+	Unsigned int: 1069547520
+	Float: 1.5
+	Hexadecimal: 0x3fc00000
+	Octal: 07760000000
+	Binary: 0b111111110000000000000000000000
+	IPv4: 63.192.0.0
+	Timestamp: Sun, 23 Nov 2003 00:32:00 UTC/GMT-0
 
 Hit Ctrl+C to quit.
 
--- examine.c
+++ examine.c
@@ -3,48 +3,26 @@
 #include <string.h>
 #include <limits.h>
 #include <time.h>
+#include <math.h>
 
 #define MAX_CMD_LINE 1000
 
-const char *nongraph[] =
-{
-   "NUL",
-   "SOH",
-   "STX",
-   "ETX",
-   "EOT",
-   "ENQ",
-   "ACK",
-   "BEL",
-   "BS",
-   "\\t (tab)",
-   "\\n (newline or line feed)",
-   "VT (vertical tab)",
-   "FF",
-   "\\r (carriage return)",
-   "SO",
-   "SI",
-   "DLE",
-   "DC1",
-   "DC2",
-   "DC3",
-   "DC4",
-   "NAK",
-   "SYN",
-   "ETB",
-   "CAN",
-   "EM",
-   "SUB",
-   "ESC",
-   "FS",
-   "GS",
-   "RS",
-   "US"
-};
+// From https://stackoverflow.com/questions/16164620/how-to-convert-an-ieee-754-single-precision-binary-floating-point-to-decimal/29969785#29969785
+float int2float(int uintS) {
+	unsigned int sign     = (uintS & 0x80000000); //01111111 10000000 00000000 00000000
+	unsigned int exponent = (uintS & 0x7F800000); //01111111 10000000 00000000 00000000
+	unsigned int mantessa = (uintS & 0x007FFFFF); //00000000 01111111 11111111 11111111
+	float normalizedExponent = (float) ((signed char) ((exponent>>23) - 127));
+	float normalizedMantessa = (float) 1 + (float) mantessa / pow((float)2,(float)23);
+	float theVar = normalizedMantessa * pow((float)2,(float)normalizedExponent);
+
+	if (sign != 0) theVar = theVar * (float) (-1);
+	if (fabs(theVar) < pow((float) 10, (float) -38)) theVar = 0;
+	return theVar;
+}
 
 // From https://stackoverflow.com/questions/1024389/print-an-int-in-binary-representation-using-c
-char *int2bin(int i)
-{
+char *int2bin(int i) {
     size_t bits = sizeof(int) * CHAR_BIT;
 
     char * str = malloc(bits + 1);
@@ -64,8 +42,8 @@ char *int2bin(int i)
 }
 
 // From https://stackoverflow.com/questions/13520067/about-rot13-implementation/14636260#14636260
-void rot13 (char *s) {
-    if (s == NULL)
+void rot13(char *s) {
+    if (!s)
         return;
 
     int i;
@@ -77,13 +55,18 @@ void rot13 (char *s) {
     }
 }
 
-void examine(const char *s)
-{
+void examine(const char *s) {
 	long number;
 	int i;
 
-	puts("[*] Handling as string:");
+	const char *nongraph[] = {
+   	"NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL",
+   	"BS", "\\t (tab)", "\\n (newline or line feed)", "VT (vertical tab)", "FF", "\\r (carriage return)", "SO", "SI",
+   	"DLE", "DC1", "DC2", "DC3", "DC4", "NAK", "SYN", "ETB",
+		"CAN", "EM", "SUB", "ESC", "FS", "GS", "RS", "US"
+	};
 
+	// Handle input as string
 	printf("Hex string: ");
 	for (i=0; i < strlen(s); i++)
 		printf("%2x", s[i]);
@@ -104,21 +87,23 @@ void examine(const char *s)
 	printf("ROT13: %s\n", t);
 	free(t);
 
+	// Handle input as number
+
+	// Binary prefix 0b is not automatically handled by strtoul() so we handle that
 	if (*s == '0' && *(s+1) == 'b')
 		number = (long) strtol(s+2, NULL, 2);
 	else
 		number = (long) strtol(s, NULL, 0);
 
-	puts("[*] Handling as number:");
-
 	printf("Signed int: %jd\n", number);
 	printf("Unsigned int: %ju\n", number);
-	printf("Float: %g\n", (double) number);
+	printf("Float: %g\n", int2float(number));
 	printf("Hexadecimal: %#02x\n", (unsigned) number);
 	printf("Octal: %#o\n", (unsigned) number);
 	char *binary = int2bin((int) number);
 	printf("Binary: 0b%s\n", binary);
 
+	// Customized ASCII representation for non graphical characters
 	if (number >= 0 && number < 128)
 	{
 		if (number == 127)
@@ -139,11 +124,9 @@ void examine(const char *s)
 			  (int)number & 0xff);
 
 	char timestr[40], u[40];
-	strftime(timestr, sizeof(timestr), "%a, %d %b %Y %H:%M:%S UTC/GMT-0",
-		gmtime((time_t *) &number));
-
+	strftime(timestr, sizeof(timestr), "%a, %d %b %Y %H:%M:%S UTC/GMT-0", gmtime((time_t *) &number));
 	snprintf(u, 39, "%s", timestr);
-	printf("Date/time stamp: %s\n", u);
+	printf("Timestamp: %s\n", u);
 }
 
 int main(int argc, char *argv[]) {
@@ -161,8 +144,7 @@ int main(int argc, char *argv[]) {
 			examine(s);
 			memset(s, 0, MAX_CMD_LINE);
 			i=-1;
-			
-				putchar('\n');
+			putchar('\n');
 		}
 		s[i] = c;
 	}
